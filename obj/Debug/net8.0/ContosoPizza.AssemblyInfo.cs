d061dd0f47528cf120c1f826ca732a38e184f351
Dr.Zeus⚡ <144681284+VictoryChibueze@users.noreply.github.com>
2025-11-18 18:44:11 +0100

    PizzaAPi

--- obj/Debug/net8.0/ContosoPizza.AssemblyInfo.cs
+++ obj/Debug/net8.0/ContosoPizza.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ContosoPizza")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2263bd787783d1d2fabda2b64b03ca814b33a65b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5de10f9aa0885b23aafaed6b741899e37a02c212")]
 [assembly: System.Reflection.AssemblyProductAttribute("ContosoPizza")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ContosoPizza")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
